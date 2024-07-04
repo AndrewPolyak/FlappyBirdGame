2e21f05984ec3e6f2bbd1b6e85a2f7b09d3935c7
AndrewPolyak <andrew.polyak23@gmail.com>
2024-07-04 17:18:47 -0600

    Pre-game menu navigation logic and event handlers built out

--- FlappyBirdGame/src/controller/AppController.java
+++ FlappyBirdGame/src/controller/AppController.java
@@ -1,5 +1,120 @@
 package controller;
 
-public class AppController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+
+/**
+ * TODO
+ */
+public class AppController implements Initializable {
+	
+	@FXML
+    private ImageView bird; // TODO
+
+    @FXML
+    private Circle birdHitBox; // TODO
+
+    @FXML
+    private Button birdOneToggleBtn; // TODO
+
+    @FXML
+    private Button birdThreeToggleBtn; // TODO
+
+    @FXML
+    private Button birdTwoToggleBtn; // TODO
+
+    @FXML
+    private ImageView bottomPipeOne; // TODO
+
+    @FXML
+    private Rectangle bottomPipeOneHitBox; // TODO
+
+    @FXML
+    private ImageView bottomPipeThree; // TODO
+
+    @FXML
+    private Rectangle bottomPipeThreeHitBox; // TODO
+
+    @FXML
+    private ImageView bottomPipeTwo; // TODO
+
+    @FXML
+    private Rectangle bottomPipeTwoHitBox; // TODO
+
+    @FXML
+    private AnchorPane gameModelScreen; // TODO
+
+    @FXML
+    private AnchorPane gameScreen; // TODO
+
+    @FXML
+    private ImageView mapOne; // TODO
+
+    @FXML
+    private Button mapOneToggleBtn; // TODO
+
+    @FXML
+    private AnchorPane mapScreen; // TODO
+
+    @FXML
+    private ImageView mapThree; // TODO
+
+    @FXML
+    private Button mapThreeToggleBtn; // TODO
+
+    @FXML
+    private ImageView mapTwo; // TODO
+
+    @FXML
+    private Button mapTwoToggleBtn; // TODO
+
+    @FXML
+    private AnchorPane menuScreen; // TODO
+
+    @FXML
+    private Text scoreCounter; // TODO
+
+    @FXML
+    private ImageView topPipeOne; // TODO
+
+    @FXML
+    private Rectangle topPipeOneHitBox; // TODO
+
+    @FXML
+    private ImageView topPipeThree; // TODO
+
+    @FXML
+    private Rectangle topPipeThreeHitBox; // TODO
+
+    @FXML
+    private ImageView topPipeTwo; // TODO
+
+    @FXML
+    private Rectangle topPipeTwoHitBox; // TODO
+    
+    private FlappyBirdMenuController menu; // TODO
+
+
+    /**
+     * TODO
+     */
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		menu = new FlappyBirdMenuController(mapOneToggleBtn, mapTwoToggleBtn, mapThreeToggleBtn, 
+                birdOneToggleBtn, birdTwoToggleBtn, birdThreeToggleBtn, mapOne, mapTwo, mapThree, bird, bird, bird);
+        
+		menu.detectInput(); // TODO
+		
+	}
 	
 }
--- FlappyBirdGame/src/controller/FlappyBirdMenuController.java
+++ FlappyBirdGame/src/controller/FlappyBirdMenuController.java
@@ -1,16 +1,69 @@
 package controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.AnchorPane;
+
 /**
  * TODO
  */
 public class FlappyBirdMenuController {
 	
-	// TODO properties
+	private Button mapOneToggleBtn; // TODO
+	private Button mapTwoToggleBtn; // TODO
+	private Button mapThreeToggleBtn; // TODO
+	
+	private Button birdOneToggleBtn; // TODO
+	private Button birdTwoToggleBtn; // TODO
+	private Button birdThreeToggleBtn; // TODO
+	
+	private ImageView mapOne; // TODO
+	private ImageView mapTwo; // TODO
+	private ImageView mapThree; // TODO
+	
+	private ImageView birdOne; // TODO
+	private ImageView birdTwo; // TODO
+	private ImageView birdThree; // TODO
+	
+	private boolean birdOneVisible; // TODO
+	private boolean birdTwoVisible; // TODO
+	private boolean birdThreeVisible; // TODO
+	
+	private AnchorPane menuScreen; // TODO
 	
 	
-	//TODO
-	public FlappyBirdMenuController() {
-		// TODO
+	/**
+	 * TODO
+	 * 
+	 * @param mapOneToggleBtn
+	 * @param mapTwoToggleBtn
+	 * @param mapThreeToggleBtn
+	 * @param birdOneToggleBtn
+	 * @param birdTwoToggleBtn
+	 * @param birdThreeToggleBtn
+	 * @param mapOne
+	 * @param mapTwo
+	 * @param mapThree
+	 * @param birdOne
+	 * @param birdTwo
+	 * @param birdThree
+	 */
+	public FlappyBirdMenuController(Button mapOneToggleBtn, Button mapTwoToggleBtn, Button mapThreeToggleBtn,
+			Button birdOneToggleBtn, Button birdTwoToggleBtn, Button birdThreeToggleBtn, ImageView mapOne,
+			ImageView mapTwo, ImageView mapThree, ImageView birdOne, ImageView birdTwo, ImageView birdThree) {
+		this.mapOneToggleBtn = mapOneToggleBtn;
+		this.mapTwoToggleBtn = mapTwoToggleBtn;
+		this.mapThreeToggleBtn = mapThreeToggleBtn;
+		this.birdOneToggleBtn = birdOneToggleBtn;
+		this.birdTwoToggleBtn = birdTwoToggleBtn;
+		this.birdThreeToggleBtn = birdThreeToggleBtn;
+		this.mapOne = mapOne;
+		this.mapTwo = mapTwo;
+		this.mapThree = mapThree;
+		this.birdOne = birdOne;
+		this.birdTwo = birdTwo;
+		this.birdThree = birdThree;
 	}
 	
 	
@@ -37,7 +90,11 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void mapOneToggleBtnHandler() {
-		// TODO
+		mapOneToggleBtn.setOnMouseClicked(e -> {
+			mapOne.setVisible(true);
+			mapTwo.setVisible(false);
+			mapThree.setVisible(false);
+		});
 	}
 	
 	
@@ -45,7 +102,11 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void mapTwoToggleBtnHandler() {
-		// TODO
+		mapTwoToggleBtn.setOnMouseClicked(e -> {
+			mapOne.setVisible(false);
+			mapTwo.setVisible(true);
+			mapThree.setVisible(false);
+		});
 	}
 	
 	
@@ -53,7 +114,11 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void mapThreeToggleBtnHandler() {
-		// TODO
+		mapThreeToggleBtn.setOnMouseClicked(e -> {
+			mapOne.setVisible(false);
+			mapTwo.setVisible(false);
+			mapThree.setVisible(true);
+		});
 	}
 	
 	
@@ -61,7 +126,15 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void birdOneToggleBtnHandler() {
-		// TODO
+		birdOneToggleBtn.setOnMouseClicked(e -> {
+			birdOne.setVisible(true);
+			birdTwo.setVisible(false);
+			birdThree.setVisible(false);
+			
+			birdOneVisible = true;
+			birdTwoVisible = false;
+			birdThreeVisible = false;
+		});
 	}
 	
 	
@@ -69,7 +142,15 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void birdTwoToggleBtnHandler() {
-		// TODO
+		birdTwoToggleBtn.setOnMouseClicked(e -> {
+			birdOne.setVisible(false);
+			birdTwo.setVisible(true);
+			birdThree.setVisible(false);
+			
+			birdOneVisible = false;
+			birdTwoVisible = true;
+			birdThreeVisible = false;
+		});
 	}
 	
 	
@@ -77,7 +158,15 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void birdThreeToggleBtnHandler() {
-		// TODO
+		birdThreeToggleBtn.setOnMouseClicked(e -> {
+			birdOne.setVisible(false);
+			birdTwo.setVisible(false);
+			birdThree.setVisible(true);
+			
+			birdOneVisible = false;
+			birdTwoVisible = false;
+			birdThreeVisible = true;
+		});
 	}
 	
 	
@@ -85,7 +174,32 @@ public class FlappyBirdMenuController {
 	 * TODO
 	 */
 	private void startGameHandler() {
-		// TODO detect space bar press to start game
+		
+		/*
+		 * The focus may be on any button on any time, so for the listener to function, this method 
+		 * will check all buttons and call the startGame method... The startGame button handler 
+		 * logic will work once the button-in-focus is found throughout the numerous function calls
+		 */
+		startGame(mapOneToggleBtn);
+		startGame(mapTwoToggleBtn);
+		startGame(mapThreeToggleBtn);
+		startGame(birdOneToggleBtn);
+		startGame(birdTwoToggleBtn);
+		startGame(birdThreeToggleBtn);
+	}
+	
+	
+	/**
+	 * TODO
+	 * 
+	 * @param button
+	 */
+	private void startGame(Button button) {
+		button.setOnKeyPressed(e -> { // If a key has been pressed while a button is in focus...
+			if (e.getCode() == KeyCode.SPACE) { // If the pressed key was the space bar...
+				System.out.println("GAME STARTED");
+			}
+		});
 	}
 	
 }
